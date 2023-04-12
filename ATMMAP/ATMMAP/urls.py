c533d5dbeee3066e8c9462024d7ef78f51a4a2a8
MarkOS5623 <markos5623@gmail.com>
2023-04-12 21:03:59 +0300

    Created login backend

--- ATMMAP/ATMMAP/urls.py
+++ ATMMAP/ATMMAP/urls.py
@@ -5,4 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('API.urls')),
+    path('Users/', include('django.contrib.auth.urls')),
+    path('Users/', include('Users.urls')),
 ]
