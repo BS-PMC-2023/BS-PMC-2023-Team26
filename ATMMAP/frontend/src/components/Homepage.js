59f95121b82fda1e6832927474c97c43b9a72b11
MarkOS5623 <markos5623@gmail.com>
2023-04-15 16:15:19 +0300

    Updates material UI and gitignore update

--- ATMMAP/frontend/src/components/Homepage.js
+++ ATMMAP/frontend/src/components/Homepage.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import { render } from "react-dom";
 
 export default class Homepage extends Component {
         constructor(props){
             super(props);
         }
         render(){
-            return <p>This is the home page</p>
+            return <p>Yellow</p>
         }
 }
