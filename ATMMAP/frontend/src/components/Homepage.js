797686768a07d82244effdde5dcf63bf77d614fb
Pavel Savelev <Pavelsav960@gmail.com>
2023-04-16 18:38:20 +0300

    added login signup homepage components

--- ATMMAP/frontend/src/components/Homepage.js
+++ ATMMAP/frontend/src/components/Homepage.js
@@ -1,10 +1,27 @@
-import React, { Component } from "react";
+import Navbar from './Navbar';
+import React from 'react';
+import '../styles/Homepage.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-export default class Homepage extends Component {
-        constructor(props){
-            super(props);
-        }
-        render(){
-            return <p>This is the home page</p>
-        }
-}
+const Homepage = () => {
+  return (
+    <Router>
+    <div className="home-page">
+      <Navbar />
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6 text-center">
+            <h1>Welcome to Professional Exchange</h1>
+            <p>Connect with professionals in your industry</p>
+            <button className="btn btn-primary btn-lg">Log In</button>
+            <button className="btn btn-secondary btn-lg">Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </Router>
+  );
+};
+
+export default Homepage;
+
