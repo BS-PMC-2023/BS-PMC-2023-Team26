bac0b5a708293d8d8546d75aad4da39cd89dc9d6
MarkOS5623 <markos5623@gmail.com>
2023-04-16 17:53:17 +0300

    Rearanging

--- ATMMAP/settings.py
+++ ATMMAP/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n$@rj!4m@6(7zjsv76+*5&kfaskchfflkrwu3zxhua#nmjk9=%'
+SECRET_KEY = 'django-insecure-bv#x0(mgop(*i_4a^f^tbnr6*+nz)7b5rlimhq8gg22r%pd42!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'users'
 ]
 
 MIDDLEWARE = [
