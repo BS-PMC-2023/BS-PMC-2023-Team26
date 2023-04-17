19086b1dd57d233aca8d8ccce6963e4899fce451
Yushko69 <amit.jakob7@gmail.com>
2023-04-17 19:50:26 +0300

    BSPMC2326-57:  userbase page admin

--- ATMMAP/ATMMAP/urls.py
+++ ATMMAP/ATMMAP/urls.py
@@ -2,10 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('API/', include('API.urls')),
     path('Users/', include('django.contrib.auth.urls')),
     path('Users/', include('Users.urls')),
     path('', include('frontend.urls')),
+    path('admin/', admin.site.urls),
 ]
