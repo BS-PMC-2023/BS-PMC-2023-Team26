46f801cef40979b5bce70a78019896df47527aa1
MarkOS5623 <markos5623@gmail.com>
2023-05-05 19:33:07 +0300

    Add error messages to sign up page

--- ATMMAP/frontend/src/components/Signuppage.js
+++ ATMMAP/frontend/src/components/Signuppage.js
@@ -10,6 +10,8 @@ const SignUp = () => {
   const [password2, setPassword2] = useState('');
   const [csrfToken, setCsrfToken] = useState('');
   const [isSuccess, setIsSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [errors, setErrors] = useState({});
 
   useEffect(() => {
     fetch('/Users/get-csrf-token/')
@@ -24,7 +26,16 @@ const SignUp = () => {
 
   const handleSignUp = (e) => {
     e.preventDefault();
-
+    const errors = {};
+    if (!username.trim()) errors.username = 'Username is required';
+    if (!email.trim()) errors.email = 'Email is required';
+    else if (!/\S+@\S+\.\S+/.test(email)) errors.email = 'Email is invalid';
+    if (!password.trim()) errors.password = 'Password is required';
+    if (!password2.trim()) errors.password2 = 'Please confirm your password';
+    else if (password !== password2) errors.password2 = 'Passwords do not match';
+    setErrors(errors);
+    if (Object.keys(errors).length > 0) return;
+    
     const formData = new FormData();
     formData.append('username', username);
     formData.append('email', email);
@@ -48,7 +59,7 @@ const SignUp = () => {
       })
       .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
-        // handle error
+        setErrorMessage('Failed to sign up. Please try again later.');
       });
   }
 
@@ -65,23 +76,28 @@ const SignUp = () => {
         <label className="signup-label">
           Username:
           <input className="signup-input" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+          {errors.username && <span className="signup-error">{errors.username}</span>}
         </label>
-        <label className="signup-label">
-          Email:
-          <input className="signup-input" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </label>
-        <label className="signup-label">
-          Password:
-          <input className="signup-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </label>
-        <label className="signup-label">
-          Confirm Password:
-          <input className="signup-input" type="password" value={password2} onChange={(e) => setPassword2(e.target.value)} />
-        </label>
-        <button className="signup-button" type="submit">Sign Up</button>
+          <label className="signup-label">
+            Email:
+              <input className="signup-input" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+              {errors.email && <span className="signup-error">{errors.email}</span>}
+          </label>
+          <label className="signup-label">
+            Password:
+              <input className="signup-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              {errors.password && <span className="signup-error">{errors.password}</span>}
+          </label>
+          <label className="signup-label">
+            Confirm Password:
+              <input className="signup-input" type="password" value={password2} onChange={(e) => setPassword2(e.target.value)} />
+              {errors.password2 && <span className="signup-error">{errors.password2}</span>}
+          </label>
+          {errorMessage && <p className="signup-error">{errorMessage}</p>}
+          <button className="signup-button" type="submit">Sign Up</button>
       </form>
     </>
-  );
-};
-
+    );
+  };
+          
 export default SignUp;
