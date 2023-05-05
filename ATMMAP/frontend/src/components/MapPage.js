b36516ec62eabbddebd048b5015707344e88fc29
ITAIDAMRI <itaida@ac.sce.ac.il>
2023-05-05 18:02:46 +0300

    find you exact location in the map and adding a navbar

--- ATMMAP/frontend/src/components/MapPage.js
+++ ATMMAP/frontend/src/components/MapPage.js
@@ -1,22 +1,64 @@
 import React, { useEffect, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function MapPage() {
   const mapRef = useRef(null);
 
   useEffect(() => {
-
     const map = L.map(mapRef.current).setView([51.505, -0.09], 13);
-
+  
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
     }).addTo(map);
-
-    L.marker([51.5, -0.09]).addTo(map).bindPopup("Hello, world!").openPopup();
+  
+    let marker;
+  
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          if (!marker) {
+            marker = L.marker([latitude, longitude]).addTo(map);
+          } else {
+            marker.setLatLng([latitude, longitude]);
+          }
+          map.setView([latitude, longitude], 13);
+          marker.bindPopup("You are here!").openPopup();
+        },
+        (error) => {
+          console.error("Error getting current position", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  
+    return () => {
+      if (marker) {
+        marker.remove();
+      }
+    };
   }, []);
+  
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand as={Link} to="/">ATM MAP</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/ExchangeRate">Exchange rate</Nav.Link>
 
-  return <div ref={mapRef} style={{ height: "100vh" }} />;
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div ref={mapRef} style={{ height: "calc(100vh - 56px)" }} />
+    </>
+  );
 }
 
 export default MapPage;
