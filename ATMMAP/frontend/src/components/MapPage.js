43d8bcc454e8e0ab21c712d936a8b02b1fc62672
ITAIDAMRI <itaida@ac.sce.ac.il>
2023-05-07 00:35:07 +0300

    show mark of tthe bank and option to filter them by city

--- ATMMAP/frontend/src/components/MapPage.js
+++ ATMMAP/frontend/src/components/MapPage.js
@@ -1,54 +1,78 @@
-import React, { useEffect, useRef, useState } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { URLSearchParams } from 'url';
+import React, { useRef, useEffect, useState } from 'react';
 
 function MapPage() {
   const mapRef = useRef(null);
-  const [banks, setBanks] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [cities, setCities] = useState([]);
+  const [currentLocation, setCurrentLocation] = useState(null);
 
   useEffect(() => {
-    const map = L.map(mapRef.current).setView([32.0853, 34.7818], 11);
+    const map = L.map(mapRef.current);
 
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    const banksUrl = 'https://data.gov.il/api/3/action/datastore_search?resource_id=1c5bc716-8210-4ec7-85be-92e6271955c2';
 
-    // Fetch bank locations from API
-    const apiUrl = 'https://data.gov.il/api/3/action/datastore_search?resource_id=1c5bc716-8210-4ec7-85be-92e6271955c2';
-    const data = {
-      resource_id: '1c5bc716-8210-4ec7-85be-92e6271955c2',
-      limit: 1000,
-    };
-    fetch(`${apiUrl}?${new URLSearchParams(data)}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.result.records); // log the bank records
-      const banks = data.result.records;
-      setBanks(banks);
-      banks.forEach(bank => {
-        const { address } = bank;
-        const geocoder = new window.google.maps.Geocoder();
-        geocoder.geocode({ address }, (results, status) => {
-          if (status === "OK") {
-            const { lat, lng } = results[0].geometry.location;
-            L.marker([lat(), lng()]).addTo(map).bindPopup(bank.branch_name_he);
-          } else {
-            console.error("Error geocoding address", status);
+    fetch(banksUrl)
+      .then(response => response.json())
+      .then(data => {
+        const banks = data.result.records;
+
+        const validBanks = banks.filter(bank => bank.X_Coordinate !== null && bank.Y_Coordinate !== null);
+
+        const xCoordinates = validBanks.map(bank => bank.X_Coordinate);
+        const yCoordinates = validBanks.map(bank => bank.Y_Coordinate);
+
+        if (validBanks.length > 0) {
+          // set the view to the first bank's coordinates and zoom level 16
+          map.setView([yCoordinates[0], xCoordinates[0]], 16);
+        }
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        }).addTo(map);
+
+        const allCities = validBanks.map(bank => bank.City);
+        const uniqueCities = [...new Set(allCities)];
+        setCities(uniqueCities);
+
+        validBanks.forEach(bank => {
+          const { X_Coordinate, Y_Coordinate, City } = bank;
+
+          if (!filter || City.includes(filter)) {
+            L.marker([Y_Coordinate, X_Coordinate]).addTo(map).bindPopup(bank.branch_name_he);
           }
         });
+      })
+      .catch(error => {
+        console.error('Error fetching bank locations', error);
       });
-    })
-    .catch(error => {
-      console.error('Error fetching bank locations', error);
-    });
+
+    // add a marker for the user's current location
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log('Current location:', position.coords.latitude, position.coords.longitude);
+        setCurrentLocation([position.coords.latitude, position.coords.longitude]);
+        L.marker([position.coords.latitude, position.coords.longitude]).addTo(map).bindPopup('You are here');
+      });
+    }
     
+
     return () => {
       map.remove();
     };
-  }, []);
+  }, [mapRef, filter]);
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const handleCityFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
 
   return (
     <>
@@ -62,18 +86,15 @@ function MapPage() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <div ref={mapRef} style={{ height: "calc(100vh - 56px)" }} />
-      {banks.length > 0 && (
-        <div style={{ position: "absolute", top: 10, left: 10, backgroundColor: "white", padding: 10 }}>
-          <h3>Bank Locations</h3>
-          <ul>
-            {banks.map(bank => (
-              <li key={bank._id}>{bank.branch_name_he}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </>
+      <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+        <input type="text" placeholder="Filter by city" onChange={handleFilterChange} />
+        <select onChange={handleCityFilterChange}>
+          <option value="">All Cities</option>
+          {cities.map(city => <option value={city}>{city}</option>)}
+        </select>
+        <div ref={mapRef} style={{ flex: "1", height: "100%" }} />
+      </div>
+</>
   );
 }
 
