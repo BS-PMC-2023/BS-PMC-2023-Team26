764d7c3d95c99533393740bce284758e1f9cc328
Pavel Savelev <Pavelsav960@gmail.com>
2023-05-15 16:43:36 +0300

    BSPMC2326-116:api for getting crpyto currency

--- ATMMAP/frontend/urls.py
+++ ATMMAP/frontend/urls.py
@@ -1,17 +1,17 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('signup/', views.index2,name='signup1'),
-    path('signin/', views.index2,name='signin1'),
-    path('map/', views.index,name='map'),
-    path('ResetRequest/', views.index,name='ResetRequest'),
-    path('ExchangeRate/', views.index,name='ExchangeRate'),
-    path('StockHistory/', views.index,name='StockHistory'),
-    path('account/', views.index,name='account'),
-    path('DeleteRequest/', views.index,name='DeleteRequest'),
-    path('EditRequest/', views.index),
-    path('reset_form/<uidb64>/<token>/', views.index, name='reset_form')
+    path("", views.index),
+    path("signup/", views.index2, name="signup1"),
+    path("signin/", views.index2, name="signin1"),
+    path("map/", views.index, name="map"),
+    path("ResetRequest/", views.index, name="ResetRequest"),
+    path("ExchangeRate/", views.index, name="ExchangeRate"),
+    path("StockHistory/", views.index, name="StockHistory"),
+    path("account/", views.index, name="account"),
+    path("DeleteRequest/", views.index, name="DeleteRequest"),
+    path("EditRequest/", views.index),
+    path("reset_form/<uidb64>/<token>/", views.index, name="reset_form"),
+    path("CryptoGraph/", views.index, name="CryptoGraph"),
 ]
